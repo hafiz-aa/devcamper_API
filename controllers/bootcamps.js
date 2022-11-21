526845f0b6ff07b6b7f5e8171af0d7c4c2db9ba5
hafiz-aa <mhafiz.aa@gmail.com>
2022-11-21 18:59:52 +0700

    Add CRUD method

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -3,15 +3,41 @@ const Bootcamp = require('../models/Bootcamp')
 // @desc		Get all bootcamps
 // @route   GET /api/vi/bootcamps
 // @access  Public
-exports.getBootcamps = (req, res, next) => {
-	res.status(200).send({ success: true, message: 'Show all bootcamps' })
+exports.getBootcamps = async (req, res, next) => {
+	try {
+		const bootcamps = await Bootcamp.find()
+		res.status(200).json({
+			success: true,
+			count: bootcamps.length,
+			data: bootcamps
+		})
+	} catch (err) {
+		res.status(400).json({
+			success: false
+		})
+	}
 }
 
 // @desc		Get single bootcamps
 // @route   GET /api/vi/bootcamps/:id
 // @access  Public
-exports.getBootcamp = (req, res, next) => {
-	res.status(200).send({ success: true, message: `Get bootcamp ${req.params.id}` })
+exports.getBootcamp = async (req, res, next) => {
+	try {
+		const bootcamp = await Bootcamp.findById(req.params.id)
+
+		if (!bootcamp) {
+			return res.status(400).json({ success: false })
+		}
+
+		res.status(200).json({
+			success: true,
+			data: bootcamp
+		})
+	} catch (err) {
+		res.status(400).json({
+			success: false
+		})
+	}
 }
 
 // @desc		Create new bootcamp
@@ -35,15 +61,50 @@ exports.createBootcamp = async (req, res, next) => {
 // @desc		Update bootcamp
 // @route   PUT /api/vi/bootcamps/:id
 // @access  Private
+exports.updateBootcamp = async (req, res, next) => {
+	try {
+		const bootcamp = await Bootcamp.findByIdAndUpdate(req.params.id, req.body, {
+			new: true,
+			runValidators: true
+		})
 
-exports.updateBootcamp = (req, res, next) => {
-	res.status(200).send({ success: true, message: `Bootcamp ${req.params.id} updated` })
+		if (!bootcamp) {
+			return res.status(400).json({
+				success: false
+			})
+		}
+		res.status(200).json({
+			success: true,
+			data: bootcamp
+		})
+	}
+	catch (err) {
+		res.status(400).json({
+			success: false
+		})
+	}
 }
 
 // @desc		Delete bootcamp
 // @route   DELETE /api/vi/bootcamps/:id
 // @access  Private
+exports.deleteBootcamp = async (req, res, next) => {
+	try {
+		const bootcamp = await Bootcamp.findByIdAndDelete(req.params.id)
 
-exports.deleteBootcamp = (req, res, next) => {
-	res.status(200).send({ success: true, message: 'Bootcamp deleted' })
+		if (!bootcamp) {
+			return res.status(400).json({
+				success: false
+			})
+		}
+		res.status(200).json({
+			success: true,
+			data: {}
+		})
+	}
+	catch (err) {
+		res.status(400).json({
+			success: false
+		})
+	}
 }
