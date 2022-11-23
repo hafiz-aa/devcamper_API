05cc9d788efa4006b4cd6c9c16149aea853a3808
hafiz-aa <mhafiz.aa@gmail.com>
2022-11-23 17:31:20 +0700

    Add update averagecost method

--- models/Course.js
+++ models/Course.js
@@ -38,4 +38,40 @@ const CourseSchema = new mongoose.Schema({
 	}
 })
 
+// Static method to get average of course tuition
+CourseSchema.statics.getAverageCost = async function (bootcampId) {
+	console.log('Calculating avg cost...'.blue)
+
+	const obj = await this.aggregate([
+		{
+			$match: { bootcamp: bootcampId }
+		},
+		{
+			$group: {
+				_id: '$bootcamp',
+				averageCost: { $avg: '$tuition' }
+			}
+		}
+	])
+
+	// Update the average cost to the database
+	try {
+		await this.model('Bootcamp').findByIdAndUpdate(bootcampId, {
+			averageCost: Math.ceil(obj[0].averageCost / 10) * 10
+		})
+	} catch (err) {
+		console.error(err)
+
+	}
+}
+
+// Call getAverageCosr after save
+CourseSchema.post('save', function () {
+	this.constructor.getAverageCost(this.bootcamp)
+})
+// Call getAverageCosr before remove
+CourseSchema.pre('remove', function () {
+	this.constructor.getAverageCost(this.bootcamp)
+})
+
 module.exports = mongoose.mongoose.model('Course', CourseSchema)
