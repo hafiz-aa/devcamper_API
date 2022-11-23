5ee3fcdbf1e56b3c804a8fd686337b13972f27f1
hafiz-aa <mhafiz.aa@gmail.com>
2022-11-23 10:50:02 +0700

    Add get courses for specific bootcamp

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -8,19 +8,27 @@ const {
 	getBootcampsInRadius
 } = require('../controllers/bootcamps')
 
+// Include other resource routers
+const courseRouter = require('./courses')
+
 const router = express.Router()
+
+// Main routers
 router
 	.route('/')
 	.get(getBootcamps)
 	.post(createBootcamp)
-
 router
 	.route('/:id')
 	.get(getBootcamp)
 	.put(updateBootcamp)
 	.delete(deleteBootcamp)
-
 router
 	.route('/radius/:zipcode/:distance')
 	.get(getBootcampsInRadius)
+// Re-route into other resource routers
+router
+	.use('/:bootcampId/courses',
+		courseRouter)
+
 module.exports = router
