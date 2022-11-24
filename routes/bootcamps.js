22a590c0f23b55118a13736ebe43f142624db462
hafiz-aa <mhafiz.aa@gmail.com>
2022-11-24 11:59:41 +0700

    Add advance results middleware

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -9,6 +9,9 @@ const {
 	uploadPhotoBootcamp
 } = require('../controllers/bootcamps')
 
+const Bootcamp = require('../models/Bootcamp')
+const advancedResults = require('../middleware/advancedResults')
+
 // Include other resource routers
 const courseRouter = require('./courses')
 
@@ -17,7 +20,7 @@ const router = express.Router()
 // Main routers
 router
 	.route('/')
-	.get(getBootcamps)
+	.get(advancedResults(Bootcamp, 'courses'), getBootcamps)
 	.post(createBootcamp)
 router
 	.route('/:id')
