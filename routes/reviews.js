f7fbd4edaa3b1358e606e1dbb944e16355bd735d
hafiz-aa <mhafiz.aa@gmail.com>
2022-11-29 09:58:16 +0700

    Get single review

--- routes/reviews.js
+++ routes/reviews.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const {
 	getReviews,
+	getReview
 } = require('../controllers/reviews')
 
 const Review = require('../models/Review')
@@ -18,6 +19,10 @@ router
 		}),
 		getReviews
 	)
+
+router
+	.route('/:id')
+	.get(getReview)
 
 
 module.exports = router
