36ce1f73d8293de55cdef454407d6289b4692ca1
hafiz-aa <mhafiz.aa@gmail.com>
2022-11-29 08:23:11 +0700

    Add CRUD method for users

--- server.js
+++ server.js
@@ -17,6 +17,7 @@ connectDB()
 // Route files
 const bootcamps = require('./routes/bootcamps')
 const courses = require('./routes/courses')
+const users = require('./routes/users')
 const auth = require('./routes/auth')
 
 
@@ -44,6 +45,7 @@ app.use(express.static(path.join(__dirname, 'public')))
 // Mount routers 
 app.use('/api/v1/bootcamps', bootcamps)
 app.use('/api/v1/courses', courses)
+app.use('/api/v1/users', users)
 app.use('/api/v1/auth', auth)
 app.use(errorHandler)
 
